bc7a737f4573b951a45be5fba30031d2ffe648a4
Anton Konkin <konkin.anton2005123123123@gmail.com>
2024-08-14 18:04:03 +0400

    added script to initialize db

--- tetra-plot/bot.py
+++ tetra-plot/bot.py
@@ -5,8 +5,11 @@ from aiogram import Bot, Dispatcher
 
 import config
 from handlers import default
+from db import initialize_db
 
 async def main():
+    if config.INIT_DB:
+        await initialize_db.init_db(config.DROP_DB_BEFORE_INIT)
     bot = Bot(token=config.BOT_TOKEN)
     dp = Dispatcher()
 
--- tetra-plot/config.py
+++ tetra-plot/config.py
@@ -2,6 +2,10 @@ import os
 
 BOT_TOKEN: str = os.getenv('BOT_TOKEN')
 
+MYSQL_HOST: str = os.getenv('MYSQL_HOST', 'localhost')
 MYSQL_PORT: int = os.getenv('MYSQL_PORT', 3306)
-MYSQL_USER: str = os.getenv('MYSQL_USER')
+MYSQL_USER: str = os.getenv('MYSQL_USER', 'root')
 MYSQL_PASSWORD: str = os.getenv('MYSQL_PASSWORD')
+
+INIT_DB: bool = bool(os.getenv('INIT_DB', 1))
+DROP_DB_BEFORE_INIT: bool = bool(os.getenv('DROP_DB_BEFORE_INIT', 0))
