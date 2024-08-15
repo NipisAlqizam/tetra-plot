e7e35a15d66edcd14ba9c424dba5c7e4d382f8c2
Anton Konkin <konkin.anton2005123123123@gmail.com>
2024-08-15 17:07:40 +0400

    added temprorary start command

--- tetra-plot/handlers/default.py
+++ tetra-plot/handlers/default.py
@@ -1,3 +1,27 @@
-from aiogram import Router
+from aiogram import Router, F
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiogram.utils.formatting import Bold, as_list, as_marked_section, as_key_value
 
 router = Router()
+
+
+@router.message(F.text, Command("start"))
+async def start(message: Message):
+    content = as_list(
+        as_marked_section(Bold("Что сделано:"), "Работа с базой данных"),
+        as_marked_section(
+            Bold("Что планируется:"),
+            "Создание серий",
+            "Ввод данных",
+            "Отображение списка измерений",
+            "Редактирование",
+            "Экспорт",
+            "Создание диаграмм",
+            "Редактирование диаграмм",
+            "Импорт",
+            "Сохранение в pdf",
+        ),
+        sep="\n\n",
+    )
+    await message.answer(**content.as_kwargs())
