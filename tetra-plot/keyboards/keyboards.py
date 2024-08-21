517307bb21a1d1a9fd55bd1fe5fd4b9671e90029
Anton Konkin <konkin.anton2005123123123@gmail.com>
2024-08-21 13:18:06 +0400

    added keyboard without factory

--- tetra-plot/keyboards/keyboards.py
+++ tetra-plot/keyboards/keyboards.py
@@ -6,3 +6,20 @@ def get_finish_keyboard() -> InlineKeyboardMarkup:
     kb = InlineKeyboardBuilder()
     kb.add(InlineKeyboardButton(text="Закончить", callback_data="finish"))
     return kb.as_markup()
+
+
+def get_pages_keyboard(
+    first_num: int = 1, max_num: int | None = None
+) -> InlineKeyboardMarkup:
+    kb = InlineKeyboardBuilder()
+    if max_num:
+        number_of_displayed_pages = min(5, max_num - first_num + 1)
+    else:
+        number_of_displayed_pages = 5
+    for i in range(first_num, first_num + number_of_displayed_pages):
+        kb.button(text=str(i), callback_data="pages" + str(i))
+    kb.button(text="<", callback_data="pages_back")
+    kb.button(text="Отмена", callback_data="pages_cancel")
+    kb.button(text=">", callback_data="pages_forward")
+    kb.adjust(number_of_displayed_pages, 3)
+    return kb.as_markup()
