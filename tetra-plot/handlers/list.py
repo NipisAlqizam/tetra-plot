76d9b55e753d4066b227903fb7e322fe206a854d
aoiblin <kirilinaulyana1602@gmail.com>
2024-09-05 21:04:06 +0300

    added measurements to /list command

--- tetra-plot/handlers/list.py
+++ tetra-plot/handlers/list.py
@@ -2,7 +2,7 @@ import logging
 
 from aiogram import Router, F
 from aiogram.filters import Command, StateFilter
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
 from aiomysql import Connection
 
@@ -10,13 +10,16 @@ from states import NewSeries, AddMeasurements
 import db
 import models
 from keyboards import get_pages_keyboard
+from keyboards import PagesCallbackFactory
 from texts import get_measurement_adding_text
 
 router = Router()
 router.message.filter(F.text)
 
 @router.message(StateFilter(None), Command("list"))
-async def list(message: Message, state: FSMContext, connection: Connection):
+async def list(message: Message, 
+               state: FSMContext, 
+               connection: Connection):
     series_by = await db.measuring.get_series_by_user_id(connection, message.chat.id)
     series_list = [] 
     series_list.append(f"Выберите серию измерений: ")
@@ -25,4 +28,13 @@ async def list(message: Message, state: FSMContext, connection: Connection):
     await message.answer('\n'.join(series_list), 
     reply_markup = get_pages_keyboard(series_by))
 
-@dp.callback_query
+@router.callback_query(PagesCallbackFactory.filter())
+async def callback_id_read(callback: CallbackQuery,
+                           callback_data: PagesCallbackFactory,
+                           connection: Connection):
+    measurements_by = await db.measuring.get_measurements(connection, callback_data.series_id)
+    measurements_list = []
+    measurements_list.append(f"Вы выбрали серию измерений {callback_data.series_id} \n\n Сохранённые данные:\n")
+    for measurement in measurements_by:
+        measurements_list.append(f"{measurement.x} {measurement.y} {measurement.comment}")
+    await callback.message.answer('\n'.join(measurements_list))
--- tetra-plot/keyboards/__init__.py
+++ tetra-plot/keyboards/__init__.py
@@ -1,1 +1,1 @@
-from .keyboards import get_finish_keyboard, get_pages_keyboard
+from .keyboards import get_finish_keyboard, get_pages_keyboard, PagesCallbackFactory
