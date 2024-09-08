d115c498c9475e2a888aa4e88845b0906f6d3452
Anton Konkin <konkin.anton2005123123123@gmail.com>
2024-09-08 16:31:00 +0300

    fixed long button loading in list message

--- tetra-plot/handlers/list.py
+++ tetra-plot/handlers/list.py
@@ -16,25 +16,35 @@ from texts import get_measurement_adding_text
 router = Router()
 router.message.filter(F.text)
 
+
 @router.message(StateFilter(None), Command("list"))
-async def list(message: Message, 
-               state: FSMContext, 
-               connection: Connection):
+async def list(message: Message, state: FSMContext, connection: Connection):
     series_by = await db.measuring.get_series_by_user_id(connection, message.chat.id)
-    series_list = [] 
+    series_list = []
     series_list.append(f"Выберите серию измерений: ")
     for i, series in enumerate(series_by, start=1):
-        series_list.append(f"{i}. {series.title} с осями {series.x_name} и {series.y_name}")
-    await message.answer('\n'.join(series_list), 
-    reply_markup = get_pages_keyboard(series_by))
+        series_list.append(
+            f"{i}. {series.title} с осями {series.x_name} и {series.y_name}"
+        )
+    await message.answer(
+        "\n".join(series_list), reply_markup=get_pages_keyboard(series_by)
+    )
+
 
 @router.callback_query(PagesCallbackFactory.filter())
-async def callback_id_read(callback: CallbackQuery,
-                           callback_data: PagesCallbackFactory,
-                           connection: Connection):
-    measurements_by = await db.measuring.get_measurements(connection, callback_data.series_id)
+async def callback_id_read(
+    callback: CallbackQuery, callback_data: PagesCallbackFactory, connection: Connection
+):
+    measurements_by = await db.measuring.get_measurements(
+        connection, callback_data.series_id
+    )
     measurements_list = []
-    measurements_list.append(f"Вы выбрали серию измерений {callback_data.series_id} \n\n Сохранённые данные:\n")
+    measurements_list.append(
+        f"Вы выбрали серию измерений {callback_data.series_id} \n\n Сохранённые данные:\n"
+    )
     for measurement in measurements_by:
-        measurements_list.append(f"{measurement.x} {measurement.y} {measurement.comment}")
-    await callback.message.answer('\n'.join(measurements_list))
+        measurements_list.append(
+            f"{measurement.x} {measurement.y} {measurement.comment}"
+        )
+    await callback.message.answer("\n".join(measurements_list))
+    await callback.answer()
